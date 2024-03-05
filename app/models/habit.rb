951babe4d4eb021e15f3c967c5a940dac44d3bab
Thomas Ellwood <taellwood@gmail.com>
2024-03-05 13:46:04 +0000

    User seeded

--- app/models/habit.rb
+++ app/models/habit.rb
@@ -3,7 +3,7 @@ class Habit < ApplicationRecord
   belongs_to :relationship
   has_many :logs
 
-  validates :title :identity_goal, :trigger, :reward, :duration_in_minutes, :week_recurrence, :current_streak, :best_streak, :days, :start_times, presence: true
+  validates :title, :identity_goal, :trigger, :reward, :duration_in_minutes, :week_recurrence, :current_streak, :best_streak, :days, :start_times, presence: true
   validates :category, presence: true, inclusion: { in: ['Giving Back', 'Family', 'Friends', 'Partner', 'Creativity', 'Travel', 'Learning', 'Well-being', 'Spirituality', 'Pets'],
      message: "%{value} is not a valid category" }
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,51 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+require 'date'
+
+puts 'destroying all Users...'
+User.destroy_all
+
+puts 'seeding Users...'
+User.new
+
+scare_crow = User.create!(
+  email: "scareyC@example.com",
+  encrypted_password: "password",
+  reset_password_token: "[reset_password_token_1]",
+  reset_password_sent_at: "[reset_password_sent_at_1]",
+  remember_created_at: "[remember_created_at_1]",
+  created_at: DateTime.new(2024, 03, 5, 16, 30, 00),
+  updated_at: DateTime.new(2024, 03, 5, 16, 30, 00),
+  first_name: "Scare",
+  last_name: "Crow",
+  nickname: "Strawbrains",
+  date_of_birth: DateTime.new(2002, 3, 15),
+  work_days_per_week: 5,
+  work_hours_per_day: 5,
+  sleep_hours_per_day: 10,
+  terms_agreed: true
+)
+
+
+tin_man = User.create!(
+    email: "TinManz@example.com",
+    encrypted_password: "password",
+    reset_password_token: "[reset_password_token_2]",
+    reset_password_sent_at: "[reset_password_sent_at_2]",
+    remember_created_at: "[remember_created_at_2]",
+    created_at: DateTime.new(2024, 03, 5, 16, 30, 00),
+    updated_at: DateTime.new(2024, 03, 5, 16, 30, 00),
+    first_name: "Tin",
+    last_name: "Man",
+    nickname: "Heartless",
+    date_of_birth: DateTime.new(1979, 5, 1),
+    work_days_per_week: 6,
+    work_hours_per_day: 10,
+    sleep_hours_per_day: 5,
+    terms_agreed: true
+  )
+
+
+puts "Users seeded successfully"
