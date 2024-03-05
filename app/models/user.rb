77a0e3094828a68f059a1f9bb9235a959eb4918a
Thomas Ellwood <taellwood@gmail.com>
2024-03-05 12:36:45 +0000

    completed validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,10 @@ class User < ApplicationRecord
 
   has_many :habits
   has_many :relationships
+
+  validates :first_name, :last_name, :date_of_birth, :work_days_per_week, :work_hours_per_day, :sleep_hours_per_day, presence: true
+
+  validates :work_days_per_week, numericality: {greater_than_or_equal_to: 0, less_than_or_equal_to: 7, message: "must be less than or equal to 7" }
+  validates :work_hours_per_day, numericality: {greater_than_or_equal_to: 0, less_than_or_equal_to: 24, message: "must be less than or equal to 24" }
+  validates :sleep_hours_per_day, numericality: {greater_than_or_equal_to: 0, message: "must be greater than 0" }
 end
