ec261f3e7b94713625dddf44638fdedcd533b5aa
Jasper Warmenhoven <jasper.warmenhoven@hotmail.com>
2024-03-05 16:06:39 +0000

    Added funtionality for menu to close when clicking outside of the menu as well as on the burger menu

--- app/javascript/controllers/toggle_menu_controller.js
+++ app/javascript/controllers/toggle_menu_controller.js
@@ -12,6 +12,20 @@ export default class extends Controller {
 
   toggleMenu(e) {
     console.log(e);
-    this.menuTarget.classList.toggle("active");
+    this.menuTarget.classList.toggle("menu-open");
+
+    if (this.menuTarget.classList.contains("menu-open")) {
+      document.addEventListener("click", this.closeMenu.bind(this), true);
+    } else {
+      document.removeEventListener("click", this.closeMenu.bind(this));
+    }
+  }
+
+  closeMenu(e) {
+    if (!this.menuTarget.contains(e.target) &&
+      !this.element.contains(e.target)) {
+      this.menuTarget.classList.remove("menu-open");
+      document.removeEventListener("click", this.closeMenu.bind(this));
+    }
   }
 }
