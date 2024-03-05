d6e4e33c4c7069a075c99ff2da24c23768146c36
Thomas Ellwood <taellwood@gmail.com>
2024-03-05 11:32:16 +0000

    added associations to models

--- app/models/habit.rb
+++ app/models/habit.rb
@@ -1,4 +1,5 @@
 class Habit < ApplicationRecord
   belongs_to :user
   belongs_to :relationship
+  has_many :logs
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_many :habits
+  has_many :relationships
 end
