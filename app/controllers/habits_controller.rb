58a44539b7072125d346839d660eb0c4f41a351a
Sean Donnelly <sean_donnelly@hotmail.co.uk>
2024-03-06 11:42:05 +0000

    Created new and create methods for Habits Controller

--- app/controllers/habits_controller.rb
+++ app/controllers/habits_controller.rb
@@ -1,2 +1,22 @@
 class HabitsController < ApplicationController
+
+  def new
+    @habit = Habit.new
+  end
+
+  def create
+    @habit = Habit.new(habit_params)
+    if @habit.save
+      redirect_to habit_path(@habit)
+    else
+      render 'lists/show', status: :unprocessable_entity
+      # Not sure what to do here. Reload page but keep values?
+    end
+  end
+
+  private
+
+  def habit_params
+    params.require(:habit).permit(:title, :category, :identity_goal, :trigger, :reward, :duration_in_minutes, :week_recurrence, :days, :start_times)
+  end
 end
