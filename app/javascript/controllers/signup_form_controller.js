64e8db5ddacb9d99e3c8e2d18e394aaa35ed393f
Rowan Heptinstall <rowanheptinstall1@gmail.com>
2024-03-07 15:00:34 +0000

    JS controller for sign-up conditional autofill function built

--- app/javascript/controllers/signup_form_controller.js
+++ app/javascript/controllers/signup_form_controller.js
@@ -16,7 +16,7 @@ export default class extends Controller {
     // console.log(this.submitBtnTarget);
     // console.log(this.sleepHrsTarget);
     // console.log(this.workHrsTarget);
-    // console.log(this.workDaysTarget);
+    console.log(this.workDaysTarget);
     console.log(this.fetchSessionData());
   }
 
@@ -25,8 +25,24 @@ export default class extends Controller {
       .then(response => response.json())
       .then(data => {
         console.log("Session data:", data);
-        console.log(data.step1);
+        this.autofillForm(data)
       })
       .catch(error => console.error("Error fetching session data:", error));
+  }
+
+  autofillForm(data) {
+    // console.log(data.step1.date_of_birth)
+    if(typeof data.step1.date_of_birth != "undefined") {
+      const dateOfBirth = new Date(data.step1.date_of_birth);
+      this.dateOfBirthTarget.value = `${dateOfBirth.getFullYear()}-${dateOfBirth.getMonth()+1}-${dateOfBirth.getDate()}`;
+    }
+
+    if(typeof data.step3.work_days_per_week != "undefined") {
+      this.workHrsTarget.value = data.step3.work_hours_per_day
+    } else if(typeof data.step3.work_hours_per_day != "undefined") {
+      this.workDaysTarget.value = data.step3.work_hours_per_day
+    } else if(typeof data.step3.sleep_hours_per_day != "undefined") {
+      this.sleepHrsTarget.value = data.step3.sleep_hours_per_day
+    }
   }
 }
