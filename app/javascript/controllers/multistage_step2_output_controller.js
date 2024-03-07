dfed9d515290e452d069419e17b2e5e0a896953b
Jasper Warmenhoven <jasper.warmenhoven@hotmail.com>
2024-03-07 13:37:19 +0000

    Updated handling of relationship to be done by one handleRelation() method given same handling in initial step

--- app/javascript/controllers/multistage_step2_output_controller.js
+++ app/javascript/controllers/multistage_step2_output_controller.js
@@ -21,48 +21,33 @@ export default class extends Controller {
       .then(data => {
         console.log(data);
         this.sessionData = data;
-        this.createOutput();
+        this.handleSessionData();
       })
       .catch(error => console.error("Error fetching session data:", error));
     }
 
-    createOutput() {
+
+
+  handleSessionData() {
+    this.handleRelation();
+
+
+    // switch(relation_to.toLowerCase()) {
+    //   case 'parent':
+    //     this.handleParent();
+    //     break;
+    //   case 'child':
+    //     this.handleChild();
+    //     break;
+    //   default:
+    //     this.handleOther();
+    // }
+  }
+
+  handleRelation() {
     const relation_to = this.sessionData.step2.relation_to;
+    console.log(`Relation is a ${relation_to}`);
 
-    switch(relation_to.toLowerCase()) {
-      case 'parent':
-        this.handleParent();
-        break;
-      case 'child':
-        this.handleChild();
-        break;
-      default:
-        this.handleOther();
-    }
-  }
-
-  handleParent() {
-    console.log("It is a parent");
-    const userDoB = new Date(this.sessionData.step1.date_of_birth);
-    const parentDoB = new Date(this.sessionData.step2.date_of_birth);
-
-    const parentAge = this.getAge(parentDoB)
-    const sharedYrs = this.totalYrs - parentAge;
-    console.log(`Shared years: ${sharedYrs}`);
-  }
-
-  handleChild() {
-    console.log("It is a child");
-    const userDoB = new Date(this.sessionData.step1.date_of_birth);
-    const childDoB = new Date(this.sessionData.step2.date_of_birth);
-
-    const userAge = this.getAge(userDoB)
-    const sharedYrs = this.totalYrs - userAge;
-    console.log(`Shared years: ${sharedYrs}`);
-  }
-
-  handleOther() {
-    console.log("It is another relation");
     const userDoB = new Date(this.sessionData.step1.date_of_birth);
     const relationDoB = new Date(this.sessionData.step2.date_of_birth);
 
@@ -76,7 +61,7 @@ export default class extends Controller {
 
     const sharedYrs = pastYrs + futureYrs;
 
-    console.log(`User is ${userAge} and relation is ${relationAge}`);
+    console.log(`User is ${userAge} and ${relation_to} is ${relationAge}`);
     console.log(`You have ${futureYrs} years left of the total ${sharedYrs} years`);
 
     this.firstScreen(sharedYrs);
