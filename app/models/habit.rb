9df4723814bf080a6d5ea993d2d769acafc1c59e
Sean Donnelly <sean_donnelly@hotmail.co.uk>
2024-03-07 14:24:52 +0000

    Completed button states

--- app/models/habit.rb
+++ app/models/habit.rb
@@ -4,7 +4,7 @@ class Habit < ApplicationRecord
   has_many :logs
   has_one_attached :photo
 
-  validates :title, :identity_goal, :trigger, :reward, :duration_in_minutes, :week_recurrence, :current_streak, :best_streak, :start_time, presence: true
+  # validates :title, :identity_goal, :trigger, :reward, :duration_in_minutes, :week_recurrence, :current_streak, :best_streak, :start_time, presence: true
   validates :category, presence: true, inclusion: { in: [
     "Parents",
     "Children",
