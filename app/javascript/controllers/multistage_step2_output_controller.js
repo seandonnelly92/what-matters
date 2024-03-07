bf910c9341690fc1aaf15fdf146f25558e84d963
Jasper Warmenhoven <jasper.warmenhoven@hotmail.com>
2024-03-07 11:27:18 +0000

    Added fetchSessionData() to stimulus controller to fetch session data

--- app/javascript/controllers/multistage_step2_output_controller.js
+++ app/javascript/controllers/multistage_step2_output_controller.js
@@ -3,5 +3,18 @@ import { Controller } from "@hotwired/stimulus"
 // Connects to data-controller="multistage-step2-output"
 export default class extends Controller {
   connect() {
+    console.log("Hello from the multistage-step2-output controller!");
+
+    console.log(this.fetchSessionData());
+  }
+
+  fetchSessionData() {
+    fetch('fetch_session_data')
+      .then(response => response.json())
+      .then(data => {
+        console.log("Session data:", data);
+        console.log(data.step1);
+      })
+      .catch(error => console.error("Error fetching session data:", error));
   }
 }
