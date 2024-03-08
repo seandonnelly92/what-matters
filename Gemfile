6ddb36ad1c333d206bbc53ad35631cb64e121ce0
Jasper Warmenhoven <jasper.warmenhoven@hotmail.com>
2024-03-08 11:36:00 +0000

    Added chartkick as well

--- Gemfile
+++ Gemfile
@@ -4,6 +4,7 @@ ruby "3.1.2"
 
 # Additional gems added for project
 gem "psych", "4.0"
+gem "chartkick"
 
 # Bundle edge Rails instead: gem "rails", github: "rails/rails", branch: "main"
 gem "rails", "~> 7.1.3", ">= 7.1.3.2"
--- app/javascript/controllers/multistage_step3_output_controller.js
+++ app/javascript/controllers/multistage_step3_output_controller.js
@@ -1,12 +1,7 @@
 import { Controller } from "@hotwired/stimulus"
 
-import { Chart } from "chart.js";
-import * as Chartjs from "chart.js";
-
-const controllers = Object.values(Chartjs).filter(
-  (chart) => chart.id !== undefined
-);
-Chart.register(...controllers);
+import Chartkick from "chartkick"
+import "chartkick/chart.js"
 
 // Connects to data-controller="multistage-step3-output"
 export default class extends Controller {
@@ -20,32 +15,7 @@ export default class extends Controller {
   connect() {
     console.log("Hello from the multistage-step3-output controller!");
 
-    const firstChart = this.timeBreakdownTarget;
-
-    const data = {
-      labels: [
-        'Red',
-        'Blue',
-        'Yellow'
-      ],
-      datasets: [{
-        label: 'My First Dataset',
-        data: [300, 50, 100],
-        backgroundColor: [
-          'rgb(255, 99, 132)',
-          'rgb(54, 162, 235)',
-          'rgb(255, 205, 86)'
-        ],
-        hoverOffset: 4
-      }]
-    };
-
-    const config = {
-      type: 'pie',
-      data: data,
-    };
-
-    new Chart(firstChart, config);
+    new Chartkick.PieChart(this.timeBreakdownTarget, [["Blueberry", 44], ["Strawberry", 23]])
 
     this.fetchSessionData();
   }
