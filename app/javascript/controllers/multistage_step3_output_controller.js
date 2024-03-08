4ca0711815acc6d2044a7647f52baf1e7fa3bfad
Jasper Warmenhoven <jasper.warmenhoven@hotmail.com>
2024-03-08 14:15:58 +0000

    Added pie charts for the step3 output

--- app/javascript/controllers/multistage_step3_output_controller.js
+++ app/javascript/controllers/multistage_step3_output_controller.js
@@ -5,7 +5,7 @@ export default class extends Controller {
   static targets = [
     "title",
     "timeBreakdown",
-    "timeBreakdownDetail",
+    "legend",
     "nextBtn"
   ]
 
@@ -20,12 +20,9 @@ export default class extends Controller {
     fetch('fetch_session_data')
       .then(response => response.json())
       .then(data => {
-        console.log("Fetching data...");
-        console.log(data);
         this.sessionData = data;
         this.breakdownCalcs();
         this.breakdownChart();
-        // this.breakdownChartDetailed();
       })
       .catch(error => console.error("Error fetching session data:", error));
   }
@@ -35,13 +32,12 @@ export default class extends Controller {
     const workDays = this.sessionData.step3.work_days_per_week;
     const workHrs = this.sessionData.step3.work_hours_per_day;
 
-    console.log(`Sleep hours is: ${sleepHrs}`);
-    console.log(`Work days is: ${workDays}`);
-    console.log(`Work hours is: ${workHrs}`);
     this.unavoidables = (7 * sleepHrs) + (workDays * workHrs);
     this.freeTime = (24 * 7) - this.unavoidables;
-    console.log(`Unavoidables is: ${this.unavoidables}`);
-    console.log(`Free time is: ${this.freeTime}`);
+
+    const annualContactDays = this.sessionData.various.annual_contact_days; // This are the annual contact days  with the user's key relationship
+    const annualContactHrs = annualContactDays * 8; // Assuming a contact day is 8 hours
+    this.keyRelationship = annualContactHrs / 52; // Reflects weekly contact hours
   }
 
   breakdownChart() {
@@ -64,37 +60,90 @@ export default class extends Controller {
     const config = {
       type: 'pie',
       data: data,
+      options: {
+        responsive: true,
+        legend: {
+            display: false,
+        }
+      }
     };
 
-    const chart = new Chart(this.timeBreakdownTarget, config);
+    this.outputChart = new Chart(this.timeBreakdownTarget, config);
   }
 
   breakdownChartDetailed() {
     const nickname = this.sessionData.step2.nickname;
 
-    const data = {
-      labels: [
+    const adjustedFreeTime = this.freeTime - this.keyRelationship;
+
+    // Assuming `this.chart` is the chart instance
+    // Update labels
+    this.outputChart.data.labels = [
         'Unavoidables',
         'Free time',
         `Weekly time w/ ${nickname}`
-      ],
-      datasets: [{
-        label: 'Time breakdown',
-        data: [this.unavoidables, this.freeTime],
-        backgroundColor: [
-          'rgb(168, 213, 186)',
-          'rgb(34, 82, 70)',
-          'rgb(255, 119, 119)'
-        ],
-        hoverOffset: 4
-      }]
-    };
+    ];
 
-    const config = {
-      type: 'pie',
-      data: data,
-    };
+    // Update datasets
+    this.outputChart.data.datasets.forEach((dataset) => {
+        dataset.data = [this.unavoidables, adjustedFreeTime, this.keyRelationship];
+        dataset.backgroundColor = [
+            'rgb(168, 213, 186)',
+            'rgb(34, 82, 70)',
+            'rgb(255, 119, 119)'
+        ];
+    });
 
-    const chart = new Chart(this.timeBreakdownTarget, config);
+    // Re-render the chart to animate the changes
+    this.outputChart.update();
+
+    // Update title and legend
+    const title = `Here is how much of that you spend with ${nickname}:`;
+    this.setTitle(title);
+    this.legendTarget.innerHTML = `<span class="third">Time w/ ${nickname}</span> vs <span class="second">other free time</span> vs <span class="first">unavoidables</span>`;
+
+    // Update the next button to be used for the finalStep()
+    this.nextBtnTarget.setAttribute('data-action', 'click->multistage-step3-output#whatMattersChart');
+  }
+
+  whatMattersChart() {
+    const nickname = this.sessionData.step2.nickname;
+
+    // Assuming `this.chart` is the chart instance
+    // Update labels
+    this.outputChart.data.labels = [
+      'Free time',
+      `Weekly time w/ ${nickname}`
+    ];
+
+    // Update datasets
+    this.outputChart.data.datasets.forEach((dataset) => {
+        dataset.data = [(this.freeTime - this.keyRelationship), this.keyRelationship];
+        dataset.backgroundColor = [
+            'rgb(34, 82, 70)',
+            'rgb(255, 119, 119)'
+        ];
+    });
+
+    // Re-render the chart to animate the changes
+    this.outputChart.update();
+
+    // Update title and legend
+    const title = `Ready to grow into new habits and refocus your freetime on what matters?`;
+    this.setTitle(title);
+    this.legendTarget.innerHTML = `<span class="third">Time w/ ${nickname}</span> and <span class="second">other free time</span>`;
+
+    // Update the next button to be used for the finalStep()
+    this.nextBtnTarget.setAttribute('data-action', 'click->multistage-step3-output#finalStep');
+    this.nextBtnTarget.innerText = 'Get Started!'
+  }
+
+  finalStep() {
+    // Redirect to sign up
+    window.location.href = '/users/sign_up';
+  }
+
+  setTitle(newTitle) {
+    this.titleTarget.innerText = newTitle;
   }
 }
