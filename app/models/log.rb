f4340bc632c491b583a6445a2cb1eaaab03c917b
Thomas Ellwood <taellwood@gmail.com>
2024-03-08 13:49:56 +0000

    changed background colours

--- app/models/log.rb
+++ app/models/log.rb
@@ -4,25 +4,13 @@ class Log < ApplicationRecord
   validates :date_time, presence: true
   validates :completed, inclusion: [true, false]
 
-# day = date_time.strftime("%d").to_i
-# formatted_date = "#{day}#{ordinal_suffix(day)} #{date_time.strftime("%B")}"
-
-# def ordinal_suffix(day)
-#   if (11..13).include?(day % 100)
-#     "th"
-#   else
-#     case day % 10
-#     when 1
-#       "st"
-#     when 2
-#       "nd"
-#     when 3
-#       "rd"
-#     else
-#       "th"
-#     end
-#   end
+# building TODAY MESSAGE
+# def today_message
+# today = Time.now
+# test_date = Log.date_time
+# "#{today} and then ##{test_date}"
 # end
+##
 
   def format_message
     day = date_time.strftime("%d").to_i
@@ -46,6 +34,6 @@ class Log < ApplicationRecord
 
     formatted_date = "#{day}#{ordinal_suffix(day)} #{date_time.strftime("%B")}"
     # completed ? "Well done! You completed on  #{date_time.strftime("%Y-%m-%d %H:%M")}" : "Not completed. Remember your trigger for this habit: #{habit.trigger}!"
-    completed ? "Well done! You completed on  #{formatted_date}" : "Not completed. Remember your trigger for this habit: #{habit.trigger}!"
+    completed ? "Well done! You completed on  #{formatted_date}": "Not completed. Remember your trigger for this habit: #{habit.trigger}!"
   end
 end
