7f323f981945a9044170f47f2519037cd550357b
Jasper Warmenhoven <jasper.warmenhoven@hotmail.com>
2024-03-09 11:43:15 +0000

    Added initial animation of circle filling upon click

--- app/javascript/controllers/log_click_controller.js
+++ app/javascript/controllers/log_click_controller.js
@@ -5,32 +5,45 @@ export default class extends Controller {
 
   static values = {
     date: String,
-    completed: String,
-    token: String,
+    completed: String
   }
 
   static targets = ["circle", "message"]
 
   connect() {
-    // console.log("Hello from logs container scroller")
-    // console.log(this.dateValue)
-    // console.log(this.completedValue)
+    console.log("Hello from logs container scroller")
+
+    // Retrieves the required CRSF token from the HTML header (used to send requests)
+    this.csrfToken = document.querySelector("meta[name='csrf-token']").getAttribute("content");
   }
 
   handleDotClick(event) {
     const logId = event.target.dataset.id;
-    console.log(logId);
+    console.log(`Log ID is: ${logId}`);
+
+    // this.circleTarget.style.transform = 'scale(1)';
+    this.circleTarget.classList.toggle("completing");
+
+    // this.circleTarget.style.transition = 'width 3s linear';
+
+  }
+
+  handleHabitComplete() {
     // Your logic to handle the click event goes here
     fetch(`/logs/${logId}`, {
       method: 'PATCH',
       "headers": {
-        "X-CSRF-Token": this.tokenValue,
+        "X-CSRF-Token": this.csrfToken,
         "Accept": "application/json"
       }
-    }).then((resp) => resp.json()).then((data) => {
-      this.messageTarget.innerText = data.message;
     })
-    this.circleTarget.classList.toggle("completed")
-    console.log(this.messageTarget)
+    .then((resp) => resp.json())
+    .then((data) => {
+      this.messageTarget.innerText = data.message;
+      console.log(this.messageTarget);
+      this.circleTarget.classList.toggle("completed");
+    })
+
   }
+
 }
