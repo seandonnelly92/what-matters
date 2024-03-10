d42d0713767bcae22155f61ef196d1a4e97005e5
Jasper Warmenhoven <jasper.warmenhoven@hotmail.com>
2024-03-10 12:50:14 +0000

    Added Review validations and associations

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,2 +1,8 @@
 class Review < ApplicationRecord
+  belongs_to :user
+
+  validates :content, :rating, presence: true
+  validates :content, length: { minimum: 5 }
+  validates :rating, numericality: { only_integer: true }
+  validates :rating, comparison: { greater_than_or_equal_to: 1, less_than_or_equal_to: 5 }
 end
