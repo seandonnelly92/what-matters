4160b15df195aae7be8840a7c4f8a6ce54a2d6ba
Jasper Warmenhoven <jasper.warmenhoven@hotmail.com>
2024-03-10 15:42:59 +0000

    Updated stimulus controller for the sortby menu to open and close on click with animation

--- app/javascript/controllers/reviews_controller.js
+++ app/javascript/controllers/reviews_controller.js
@@ -2,6 +2,23 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="reviews"
 export default class extends Controller {
+  static targets = [
+    "menu"
+  ]
+
   connect() {
+    console.log("Hello from the reviews controller!");
+  }
+
+  sortbyMenu() {
+    if (this.menuTarget.classList.contains('show')) {
+      this.menuTarget.classList.remove('show')
+      setTimeout(() => {
+        this.menuTarget.classList.remove('visible')
+      }, 300); // Set timeout equal to the transition of the menu
+    } else {
+      this.menuTarget.classList.add('visible')
+      this.menuTarget.classList.add('show')
+    }
   }
 }
