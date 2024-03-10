9214ea629173903a9bd02d1f877c46dcb6a9ea12
Jasper Warmenhoven <jasper.warmenhoven@hotmail.com>
2024-03-10 13:40:09 +0000

    Added migration for users reference in the reviews table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_03_10_124301) do
+ActiveRecord::Schema[7.1].define(version: 2024_03_10_133823) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -96,6 +96,8 @@ ActiveRecord::Schema[7.1].define(version: 2024_03_10_124301) do
     t.integer "rating", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id", null: false
+    t.index ["user_id"], name: "index_reviews_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -124,4 +126,5 @@ ActiveRecord::Schema[7.1].define(version: 2024_03_10_124301) do
   add_foreign_key "habits", "users"
   add_foreign_key "logs", "habits"
   add_foreign_key "relationships", "users"
+  add_foreign_key "reviews", "users"
 end
