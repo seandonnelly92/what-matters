3c619c4dac517b9048c12247f9ed4bf7de94d56f
Thomas Ellwood <taellwood@gmail.com>
2024-03-11 11:32:08 +0000

    written logic to achieve 'streak' message on Habit card

--- app/controllers/logs_controller.rb
+++ app/controllers/logs_controller.rb
@@ -6,10 +6,12 @@ class LogsController < ApplicationController
 
     respond_to do |format|
       format.json do
-        render json: { message: log.format_message, habit: log.habit.title }
+        render json: { message: log.format_message, habit: log.habit.title}
       end
     end
   end
+
+
 
   private
 
