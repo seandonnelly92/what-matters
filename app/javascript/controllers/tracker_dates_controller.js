20834114a6824efc5f0039429ae24b7eafd777d7
Jasper Warmenhoven <jasper.warmenhoven@hotmail.com>
2024-03-11 16:51:25 +0000

    Initalised tracker-dates controller

--- app/javascript/controllers/tracker_dates_controller.js
+++ app/javascript/controllers/tracker_dates_controller.js
@@ -2,6 +2,12 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="tracker-dates"
 export default class extends Controller {
+  static targets = [
+    "datesCarousel"
+  ]
+
   connect() {
+    console.log("Hello from tracker-dates controller!");
+    console.log(this.datesCarouselTarget);
   }
 }
