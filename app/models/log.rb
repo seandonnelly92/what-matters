11227dabe3de0b6db7d541fc5e388abf5766345a
Thomas Ellwood <taellwood@gmail.com>
2024-03-13 11:05:54 +0000

    refactored format-date-message method

--- app/models/log.rb
+++ app/models/log.rb
@@ -4,18 +4,6 @@ class Log < ApplicationRecord
   validates :date_time, presence: true
 
   def format_message
-
-    if date_time.to_date < Time.now.to_date
-      completed ? "#{habit.title.capitalize}": "'#{habit.title.capitalize}' not completed for"
-    elsif date_time.to_date == Time.now.to_date
-      completed ? "#{habit.title.capitalize}": " Remember to '#{habit.title.capitalize}' today!' You set your trigger to be '#{habit.trigger}'"
-    else date_time.to_date > Time.now.to_date
-      # if habit.duration_in_minutes.present?
-      #   "#{habit.title.capitalize} for #{habit.duration_in_minutes}"
-      # end
-      "#{habit.title.capitalize} at #{habit.trigger}"
-    end
-
     "#{habit.title.capitalize}"
   end
 
@@ -37,12 +25,16 @@ class Log < ApplicationRecord
         end
       end
     end
+  end
 
     day = date_time.strftime("%d").to_i
     formatted_date = "#{day}#{ordinal_suffix(day)} #{date_time.strftime("%B")}"
 
-    "on #{formatted_date}"
-  end
-
-
+    if date_time.to_date < Time.now.to_date
+      completed ? "completed on #{formatted_date} for #{habit.duration_in_minutes} minutes": "not completed on #{formatted_date} for #{habit.duration_in_minutes} minutes"
+    elsif date_time.to_date == Time.now.to_date
+      completed ? "completed today, well done!": " Remember to '#{habit.title.capitalize}' today!' You set your trigger to be '#{habit.trigger}'"
+    else date_time.to_date > Time.now.to_date
+      "on #{formatted_date} for #{habit.duration_in_minutes} minutes"
+    end
 end
