11a55a0b43d3a67df19789abab73d1aa68a29999
Thomas Ellwood <taellwood@gmail.com>
2024-03-13 11:14:45 +0000

    debugged issues with 'habit-minutes'

--- app/models/log.rb
+++ app/models/log.rb
@@ -4,7 +4,20 @@ class Log < ApplicationRecord
   validates :date_time, presence: true
 
   def format_message
+
+    if date_time.to_date < Time.now.to_date
+      completed ? "#{habit.title.capitalize}": "'#{habit.title.capitalize}"
+    elsif date_time.to_date == Time.now.to_date
+      completed ? "#{habit.title.capitalize}": " Remember to '#{habit.title.capitalize}' today!"
+    else date_time.to_date > Time.now.to_date
+    #   if habit.duration_in_minutes.present?
+    #     "#{habit.title.capitalize} for #{habit.duration_in_minutes}"
+    #   end
+    #   "#{habit.title.capitalize}"
+    # end
+
     "#{habit.title.capitalize}"
+    end
   end
 
   def format_date_message
@@ -25,16 +38,14 @@ class Log < ApplicationRecord
         end
       end
     end
-  end
+
+
 
     day = date_time.strftime("%d").to_i
     formatted_date = "#{day}#{ordinal_suffix(day)} #{date_time.strftime("%B")}"
 
-    if date_time.to_date < Time.now.to_date
-      completed ? "completed on #{formatted_date} for #{habit.duration_in_minutes} minutes": "not completed on #{formatted_date} for #{habit.duration_in_minutes} minutes"
-    elsif date_time.to_date == Time.now.to_date
-      completed ? "completed today, well done!": " Remember to '#{habit.title.capitalize}' today!' You set your trigger to be '#{habit.trigger}'"
-    else date_time.to_date > Time.now.to_date
-      "on #{formatted_date} for #{habit.duration_in_minutes} minutes"
-    end
+    "on #{formatted_date} for #{habit.duration_in_minutes} minutes"
+  end
+
+
 end
