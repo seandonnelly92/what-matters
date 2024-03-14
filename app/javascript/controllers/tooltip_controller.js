402bc058d63a4d07410503eef7e9793f8490fed4
Jasper Warmenhoven <jasper.warmenhoven@hotmail.com>
2024-03-14 10:58:30 +0000

    Updated tooltips in habit creation to fix issue of not appearing on click

--- app/javascript/controllers/tooltip_controller.js
+++ app/javascript/controllers/tooltip_controller.js
@@ -4,26 +4,28 @@ export default class extends Controller {
   static targets = ["tticon", "ttcard", "ttcross"]
 
   connect() {
-    console.log("Hello from the tooltip controller");
+    console.log('Hello from the tooltip controller');
   }
 
   open(event) {
-    console.log("Tooltip clicked to open");
-    this.ttcardTarget.classList.remove("d-none");
+    console.log('Tooltip clicked to open');
+    this.ttcardTarget.classList.add('display-message');
+    console.log(this.ttcardTarget);
+    // debugger
   }
 
   close(event) {
     const clickedElement = event.target;
     if (!clickedElement.closest('.tooltip-card')) {
-      this.ttcardTarget.classList.add("d-none")
+      this.ttcardTarget.classList.remove('display-message')
     }
-    console.log("Tooltip clicked to close")
+    console.log('Tooltip clicked to close')
   }
 
   cross(event) {
     if (event.Target = this.ttcrossTarget) {
-      console.log("cross clicked")
-      this.ttcardTarget.classList.add("d-none")
+      console.log('cross clicked')
+      this.ttcardTarget.classList.remove('display-message')
     }
   }
 }
