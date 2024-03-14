377553f78a7875a413648121d21eca36fbcd52f8
Sean Donnelly <sean_donnelly@hotmail.co.uk>
2024-03-14 14:13:49 +0000

    Updated registrations controller for re routing

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,22 +1,15 @@
 class RegistrationsController < Devise::RegistrationsController
-<<<<<<< HEAD
+
   protected
 
-  # WHEN A USER SIGNS UP, THEY ARE REDIRECT TO ADD A HABIT
+  # WHEN A USER SIGNS IN, THEY ARE REDIRECTED TO TRACKER
   def after_sign_up_path_for(resource)
     new_habit_path
   end
 
   # WHEN A USER SIGNS IN, THEY ARE REDIRECTED TO TRACKER
   def after_sign_in_path_for(resource)
-    '/habits'
+    habits_tracker_path
   end
-=======
 
-  protected
-
-  def after_sign_up_path_for(resource)
-    new_habit_path
-  end
->>>>>>> fa2b532ded049697e65e7e05fca84599c058e932
 end
