669893127bcabfb3ca05a6f69130905f4ea576f1
Thomas Ellwood <taellwood@gmail.com>
2024-03-23 18:01:38 +0000

    button now changes 'completed' status of logs

--- app/controllers/logs_controller.rb
+++ app/controllers/logs_controller.rb
@@ -1,2 +1,14 @@
 class LogsController < ApplicationController
+  def update
+    log = Log.find(params[:id])
+    log.completed = !log.completed
+    log.save
+    head 200
+  end
+
+  private
+
+  def log_params
+    params.require(:log).permit(:completed)
+  end
 end
